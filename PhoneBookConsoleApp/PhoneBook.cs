2e9e6430aa7f7f901d12cc8a1018cc49881ecc6a
samiksha144 <samiksha144@gmail.com>
2022-06-01 13:50:00 +0530

    added

---
+++ PhoneBookConsoleApp/PhoneBook.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookConsoleApp
+{
+    class PhoneBook
+    {
+        private List<Contact> contact;
+
+        private List<Contact> _contacts { get; set; } = new List<Contact>();
+
+        private void DisplayContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact: {contact.Name},{contact.Number}");
+        }
+        private void DisplayContactDetails(List<Contact> contacts)
+        {
+            foreach (var contact in contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+        public void AddContact(Contact contact)
+        {
+            _contacts.Add(contact);
+        }
+
+        public void DisplayContact(string number)
+        {
+            var contact = _contacts.FirstOrDefault(c => c.Number == number);
+            if (contact == null)
+            {
+                Console.WriteLine("Contact not found");
+            }
+            else
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayAllContacts()
+        {
+            foreach (var contact in _contacts)
+            {
+                DisplayContactDetails(contact);
+            }
+        }
+
+        public void DisplayMatchingContacts(string searchPhrase)
+        {
+            var matchingContacts = _contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+            DisplayContactDetails(matchingContacts);
+        }
+    }
+}
